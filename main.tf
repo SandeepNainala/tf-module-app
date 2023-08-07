6edf34d4a2cc44aa77e536f94c1bc3244e25cf24
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-08-07 13:21:28 +0530

    Terraform - roboshop - autoscaling tempplate

--- main.tf
+++ main.tf
@@ -1,7 +1,42 @@
+resource "aws_security_group" "sg" {
+  name    = "${var.name}-${var.env}-sg"
+  description = "${var.name}-${var.env}-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "APP"
+    from_port = 8080
+    protocol  = "tcp"
+    to_port   = 8080
+    cidr_blocks = var.allow_app_cidr
+  }
+
+  ingress {
+    description = "SSH"
+    from_port = 22
+    protocol  = "tcp"
+    to_port   = 22
+    cidr_blocks = var.bastion_cidr
+  }
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.name}-${var.env}-sg"
+  }
+
+}
+
 resource "aws_launch_template" "template" {
   name_prefix = "${var.name}-${var.env}"
   image_id = data.aws_ami.ami.id
   instance_type = var.instance_type
+  vpc_security_group_ids = [ aws_security_group.sg.id ]
 }
 
 resource "aws_autoscaling_group" "bar" {
