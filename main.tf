32d31ff5ea1ecff65bdc5e253df5287c90335bdc
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-08-07 12:46:29 +0530

    Terraform - roboshop - autoscaling tempplate

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
-resource "aws_instance" "test" {
-  ami  = "ami-03265a0778a880afb"
+resource "aws_launch_template" "template" {
+  name_prefix = "${var.name}-${var.env}"
+  image_id = data.aws_ami.ami.id
   instance_type = var.instance_type
-  subnet_id = var.subnet_id
 }
 
-variable "instance_type" {}
-variable "subnet_id" {}
+resource "aws_autoscaling_group" "bar" {
+  availability_zones = ["us-east-1"]
+  desired_capacity = 1
+  max_size = 1
+  min_size = 1
+}
+
---
+++ vars.tf
@@ -0,0 +1,4 @@
+variable "instance_type" {}
+variable "subnet_ids" {}
+variable "name" {}
+variable "env" {}
