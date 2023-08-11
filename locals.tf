b26af8cae16acd1cdbe0f2d7809a7e4f2be39746
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-08-11 11:31:09 +0530

    Terraform - roboshop - application load balance - alb listener - target group assigned to auto scaling- listener

--- locals.tf
+++ locals.tf
@@ -1,3 +1,5 @@
 locals {
   asg_tags = merge(var.tags,{ Name = "${var.name}-${var.env}"})
+
+  dns_name = "${var.name}-${var.env}-${var.domain_name}"
 }
--- main.tf
+++ main.tf
@@ -69,4 +69,21 @@ resource "aws_lb_target_group" "main" {
 }
 
 
+resource "aws_lb_listener_rule" "rule" {
+  listener_arn = var.listener_arn
+  priority     = var.priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
+  condition {
+    host_header {
+      values = [local.dns_name]
+    }
+  }
+}
+
+
 
